af94774a85ba09d9005d41d4c545a4c8557953c0
Choiinsung <redfox276@naver.com>
2021-08-18 15:15:50 +0900

    register quiz, api

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from  .models import Quiz
 # Register your models here.
+# 관리자 페이지에 퀴즈 모델을 등록하여 관리할수 있도록 처리
+# 추가하고 python manage.py makemigrations
+admin.site.register(Quiz)
--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,18 @@
+import random
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Quiz
 from .serializers import QuizSerializer
+import random
 
 @api_view(['GET'])
 def helloAPI(request):
     return Response("hello world!")
+#개수만큼 랜덤한 퀴즈를 반환하는 api
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuizs, many=True) # 다수의 데이터에 대해서도 직렬화 가능
+    return Response(serializer.data)
